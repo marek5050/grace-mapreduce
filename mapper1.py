34aa7490522377a767dc285e47029a7bcddf3997
deborahchem@gmail.com <deborahchem@gmail.com@892991b2-2341-996a-f4a1-27958a741771>
2011-07-06 12:11:19 +0000

    first try reducer changed mapper

--- mapper1.py
+++ mapper1.py
@@ -2,7 +2,7 @@
 import sys  
 # input comes from STDIN (standard input) 
 #line = "foo foo quux labs foo bar quux" 
-#test = "2 5 15 27 39 55 100 45 78 97"
+#centroid = "2 27 100 45 78 97"
 #centroid = "(5,2) (11,4) (25,9) (69,49) (92,45)" 
 centroid = "5,4 11,10 25,11 69,67 92,78" 	
 for point in sys.stdin: 
@@ -28,15 +28,20 @@ for point in sys.stdin:
 			vSplit = value.split(',')
 			#print cSplit[0]
 			#print vSplit[0]
-			dist = (((int(cSplit[0]) - int(vSplit[0]))**2) + ((int(cSplit[1]) - int(vSplit[1]))**2))**.5
-			#print dist
-			if dist < minDist:
-				minDist = dist
-				bestCent = c
+			# To handle non-numeric value in centroid or input points
+			try:
+				#dist = abs(int(cSplit[0]) - int(vSplit[0]))
+				dist = (((int(cSplit[0]) - int(vSplit[0]))**2) + ((int(cSplit[1]) - int(vSplit[1]))**2))**.5
+				#print dist
+				if dist < minDist:
+					minDist = dist
+					bestCent = c
+			except ValueError:
+				pass
 					  
     # write the results to STDOUT (standard output);  
     # what we output here will be the input for the  
     # Reduce step, i.e. the input for reducer.py  
     #  
     # tab-delimited; the trivial word count is 1  
-       	 	print '%s\t%s' % (value, bestCent) 
+       	 	print '%s\t%s' % (bestCent, value) 
