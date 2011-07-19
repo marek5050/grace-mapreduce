cb304615d6f9c5472cf819bcc5c828d9f81d534b
deborahchem@gmail.com <deborahchem@gmail.com@892991b2-2341-996a-f4a1-27958a741771>
2011-07-19 21:00:25 +0000

    standardize datapoints

--- trunk/startup.py
+++ trunk/startup.py
@@ -10,15 +10,29 @@ def main(args):
     DP = open("datapoints.txt","w").close()
     num_points=0
     pointx = ''
-    while num_points <5000:
+    while num_points <2000:
         num_points +=500
         coords, lower, upper = 2,1, 900
     	# Create num_points random Points in n-dimensional space
     	pointx = makeRandomPoint(num_points, lower, upper)
     	shutil.copy("centroid2d.txt","centroidinput.txt")
-        os.system("python hello.py")
+        os.system("python kmeans.py")
 
-        newfilename = "statistics" + `num_points` +".txt"
+        newfilename = "statistics_" + `num_points` +".txt"
+        os.rename("statistics.txt", newfilename)
+
+    DP = open("datapoints.txt","w").close()
+    num_points=0
+    pointx = ''
+    while num_points <2000:
+        num_points +=500
+        coords, lower, upper = 3,1, 900
+    	# Create num_points random Points in n-dimensional space
+    	pointx = makeRandom3dPoint(num_points, lower, upper)
+    	shutil.copy("centroid3d.txt","centroidinput.txt")
+        os.system("python kmeans3d.py")
+
+        newfilename = "statistics3d_" + `num_points` +".txt"
         os.rename("statistics.txt", newfilename)
 
 def makeRandomPoint(num_points, lower, upper):
@@ -31,6 +45,24 @@ def makeRandomPoint(num_points, lower, upper):
 	datapoints +=`coordX`+','+`coordY`
 	datapoints += ' '
     DP = open("datapoints.txt","a")
+    # Write all the lines for datapoints at once:
+    DP.writelines(datapoints)
+    DP.close()
+
+    return datapoints
+
+def makeRandom3dPoint(num_points, lower, upper):
+    datapoints = ''
+    coordX = ''
+    coordY = ''
+    coordZ = ''
+    for i in range(num_points):
+	coordX=random.randint(lower, upper)
+ 	coordY=random.randint(lower, upper)
+	coordZ=random.randint(lower, upper)
+	datapoints +=`coordX`+','+`coordY`+','+`coordZ`
+	datapoints += ' '
+    DP = open("datapoints.txt","w")
     # Write all the lines for datapoints at once:
     DP.writelines(datapoints)
     DP.close()
