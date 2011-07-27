a53bd4311d3b320c37104e266cd8004cae8bdfa6
deborahchem@gmail.com <deborahchem@gmail.com@892991b2-2341-996a-f4a1-27958a741771>
2011-07-27 23:17:05 +0000

    iteration for 3d points

--- startup1.py
+++ startup1.py
@@ -50,7 +50,6 @@ def main(args):
             #Renaming and finalizing of files
             newfilename = "statistics_" + `num_points` +"_"+ `num_iter` +".txt"
             os.rename("statistics.txt", newfilename)
-
             newname = "cluster1_" + `num_points` +".txt"
             os.rename("cluster1.txt", newname)
             newname = "cluster2_" + `num_points` +".txt"
@@ -64,34 +63,61 @@ def main(args):
             newname = "cluster6_" + `num_points` +".txt"
             os.rename("cluster6.txt", newname)
 
-#    DP = open("datapoints.txt","w").close()
+    DP = open("datapoints.txt","w").close()
+    SP = open("stat_plot.txt","w").close()
+    ST = open("statistics.txt","w").close()
     num_points=0
     pointx = ''
-    while num_points <0:
+    while num_points <3000:
+        print "num of points"
+        print num_points
+        num_iter = 0
         num_points +=500
+        num_generated = 500
 #        # Create num_points random Points in n-dimensional space
 
-        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.4), 2,1, 900, 1,900,1,900
+        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.4), 2,1, 900, 1,900,1,900
         pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,50, 300, 50,450,50,600
+        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,50, 300, 50,450,50,600
 
     	pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-    	num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,410, 600,10,550,100,200
+    	num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,410, 600,10,550,100,200
         pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-    	num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,600, 890, 0,200,10,300
+    	num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,600, 890, 0,200,10,300
     	pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
 
-        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,100, 500,650,900,600,700
+        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,100, 500,650,900,600,700
         pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,650, 880,50,470,800,900
+        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,650, 880,50,470,800,900
         pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_points*.1), 2,800, 900,750,900,800,900
+        num_gen, coords, lowerx, upperx, lowery, uppery,lower,upper = int(num_generated*.1), 2,800, 900,750,900,800,900
         pointx = makeRandom3dPoint(num_gen, lowerx, upperx, lowery, uppery,lower,upper)
-       	shutil.copy("centroid3d.txt","centroidinput.txt")
-        os.system("python kmeans3d.py")
 
-        newfilename = "statistics3d_" + `num_points` +".txt"
-        os.rename("statistics.txt", newfilename)
+        while num_iter <5:
+            num_iter +=1
+
+            filename = "centroid3d_" + `num_iter` +".txt"
+    	    shutil.copy(filename,"centroidinput.txt")
+    	    datafilename = "datapoints3d" + `num_points` + ".txt"
+    	    #shutil.copy("datapoints1000.txt","datapoints.txt")
+    	    shutil.copy("datapoints.txt", datafilename)
+            kmeans.main(num_points)
+            # final clustering of the datapoints
+            os.system("python mapperfinal.py")
+            #Renaming and finalizing of files
+            newfilename = "statistics3d_" + `num_points` +"_"+ `num_iter` +".txt"
+            os.rename("statistics.txt", newfilename)
+            newname = "cluster3d1_" + `num_points` +".txt"
+            os.rename("cluster1.txt", newname)
+            newname = "cluster3d2_" + `num_points` +".txt"
+            os.rename("cluster2.txt", newname)
+            newname = "cluster3d3_" + `num_points` +".txt"
+            os.rename("cluster3.txt", newname)
+            newname = "cluster3d4_" + `num_points` +".txt"
+            os.rename("cluster4.txt", newname)
+            newname = "cluster3d5_" + `num_points` +".txt"
+            os.rename("cluster5.txt", newname)
+            newname = "cluster3d6_" + `num_points`
 
 def makeRandomPoint(num_points, lowerx, upperx, lowery, uppery):
     datapoints = ''
